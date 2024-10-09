34739455b401edd7857bad588e3ab80882e7171a
Milan3112 <97899861+Milan3112@users.noreply.github.com>
2024-10-09 10:54:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+new hoverEffect({
+    parent: document.querySelector('.app'),
+    intensity: 0.2,
+    image1: './ny-info.jpg',
+    image2: './ny.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+
+new hoverEffect({
+    parent: document.querySelector('.web'),
+    intensity: 0.2,
+    image1: './wolf2.png',
+    image2: './wolf.png',
+    angle: Math.PI / 8,
+    displacementImage: './12.jpg'
+});
+
+new hoverEffect({
+    parent: document.querySelector('.rab'),
+    intensity: 0.2,
+    image1: './Thanos.png',
+    image2: './hiclipart.com.png',
+    //angle: Math.PI / 4,
+    displacementImage: './14.jpg'
+});
+
+new hoverEffect({
+    parent: document.querySelector('.sab'),
+    intensity: 0.8,
+    image1: './plant.png',
+    image2: './plant2.png',
+    //angle: Math.PI / 8,
+    displacementImage: './fluid.jpg'
+});
